561588c804c5099423cee68b0b7bf2a061763fe2
Antonio Aloisio <gnuton@gnuton.org>
2018-04-21 08:52:38 +0300

    buttons moved on the left and use icons

--- UIList.py
+++ UIList.py
@@ -3,7 +3,6 @@
     Copy and paste this code in the blender code editor.
     A new panel containining a List will appear in Properties > Scene.
 """
-
 import bpy
 import bpy.props as prop
 
@@ -26,12 +25,13 @@ class MY_UL_List(bpy.types.UIList):
     def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index):
 
         # We could write some code to decide which icon to use here...
-        custom_icon = 'OBJECT_DATAMODE'
+        custom_icon = 'FORCE_LENNARDJONES'
 
         # Make sure your code supports all 3 layout types
         if self.layout_type in {'DEFAULT', 'COMPACT'}:
             layout.label(item.name, icon = custom_icon)
-            layout.label(item.random_prop ,icon = 'WORLD')
+            layout.label(item.random_prop)
+            layout.label(text = '', icon = 'WORLD')
             
         elif self.layout_type in {'GRID'}:
             layout.alignment = 'CENTER'
@@ -139,14 +139,14 @@ class PT_ListExample(bpy.types.Panel):
         row = layout.row()
         row.template_list("MY_UL_List", "The_List", scene, "my_list", scene, "list_index" )
 
-        row = layout.row()
-        row.operator('my_list.new_item', icon='NEW', text='NEW')         
-        row.operator('my_list.delete_item', text='REMOVE')         
-        row.operator('my_list.move_item', text='UP').direction = 'UP'       
-        row.operator('my_list.move_item', text='DOWN').direction = 'DOWN'         
+        col = row.column(align=True)
+        col.operator('my_list.new_item', icon='ZOOMIN', text='')         
+        col.operator('my_list.delete_item', icon='ZOOMOUT', text='')         
+        col.operator('my_list.move_item', icon='TRIA_UP', text='').direction = 'UP'       
+        col.operator('my_list.move_item', icon='TRIA_DOWN', text='').direction = 'DOWN'         
             
         if scene.list_index >= 0 and len(scene.my_list) > 0:
-            item = scene.my_list[scene.list_index-1]
+            item = scene.my_list[scene.list_index]
             row = layout.row()
             row.prop(item, "name")
             row.prop(item, "random_prop")
@@ -155,7 +155,7 @@ def register():
     bpy.utils.register_module(__name__)
     bpy.types.Scene.my_list = prop.CollectionProperty(type = ListItem)
     bpy.types.Scene.list_index = prop.IntProperty(name = "Index for my_list", default = 0)
-
+ 
 def unregister():
     del bpy.types.Scene.my_list
     del bpy.types.Scene.list_index
